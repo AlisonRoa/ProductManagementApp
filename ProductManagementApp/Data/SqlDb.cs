114259d30681ef68284550e43d0591cc98aa51b6
Alison Judith Macis Roa <rjalison37@gmail.com>
2025-09-09 15:29:51 -0600

    Agregar archivos de proyecto.

---
+++ ProductManagementApp/Data/SqlDb.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+
+
+namespace ProductManagementApp.Data
+{
+    public static class SqlDb
+    {
+        private static readonly string _cs = ConfigurationManager.ConnectionStrings["PM"].ConnectionString;
+
+        public static SqlConnection GetOpen()
+        {
+            var cn = new SqlConnection(_cs);
+            cn.Open();
+            return cn;
+        }
+
+        public static async Task<bool> TestAsync()
+        {
+            using (var cn = new SqlConnection(_cs))
+            using (var cmd = new SqlCommand("SELECT 1", cn))
+            {
+                await cn.OpenAsync();
+                var x = await cmd.ExecuteScalarAsync();
+                return (int)x == 1;
+            }
+        }
+    }
+}
